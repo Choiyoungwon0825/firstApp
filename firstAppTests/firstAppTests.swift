f8e32523315a5f18c5afdd0ded154f13c66eb429
Choiyoungwon0825 <fnul1005@naver.com>
2024-09-04 18:10:51 +0900

    Initial Commit

---
+++ firstAppTests/firstAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  firstAppTests.swift
+//  firstAppTests
+//
+//  Created by 최영원 on 9/4/24.
+//
+
+import Testing
+
+struct firstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
